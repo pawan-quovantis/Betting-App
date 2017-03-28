891384b2796db66bed910e73158c5091ac5f64ac
Vivek <vivek@quovantis.com>
2017-03-29 00:33:28 +0530

    redundant code removed.

--- resources/login.py
+++ resources/login.py
@@ -1,9 +1,7 @@
 import json
 import requests
-from flask_restful import reqparse, abort, Api, Resource
-from flask import Flask, request, redirect
-from flask_login import (LoginManager, current_user, login_required,
-                             login_user, logout_user, UserMixin, AnonymousUserMixin)
+from flask_restful import reqparse, Resource
+from flask import request
 from models import *
 from server import app, db
 from flask import jsonify
@@ -13,58 +11,16 @@ parser = reqparse.RequestParser()
 parser.add_argument('task')
 
 
-class User(UserMixin):
-    """User Session management Class
-    """
-    def __init__(self, email, id, fname="", lname="", accesstoken="", active=True):
-        self.email = email
-        self.id = id
-        self.active = active
-        self.fname = fname
-        self.lname = lname
-        self.accesstoken = accesstoken
-
-
-USERS = {1: User("anurag@grexit.com", 1, "Anurag", "Maher", "", True)}
-
-USER_NAMES = dict((u.email, u) for u in USERS.itervalues())
-
-
-class Anonymous(AnonymousUserMixin):
-    name = u"Anonymous"
-
-
+# test route
 @app.route("/")
 def hello():
-    if current_user.is_authenticated:
-        return " User " + str(current_user.myemail()) + " is logged in "
-
     return "Hello World!"
 
-
-@app.route("/logout", methods=["GET"])
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
-
-
-# @app.route("/login", methods=["GET", "POST"])
 
 class Login(Resource):
 
     def get(self):
         from modules.oauth2 import GeneratePermissionUrl
-        # if current_user.is_authenticated:
-        #     return current_user.get_id()
-        #
-        # if request.method == "GET":
-        #     useremail = request.args.get('email', '')
-        #     if useremail:
-        #         if useremail in USER_NAMES:
-        #             loginit = login_user(USER_NAMES[useremail], remember="yes")
-        #             return "user already exists and logged in"
-
         url = GeneratePermissionUrl(app.config['GOOGLE_CLIENT_ID'], request.args.get('email', ''),
                                     redirect_uri=app.config['REDIRECT_URI'],
                                     google_account_base_url=app.config['GOOGLE_ACCOUNTS_BASE_URL'])
@@ -107,15 +63,3 @@ def oauth2callback():
     else:
         return "Ony GET requests are allowed"
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.anonymous_user = Anonymous
-
-
-@login_manager.user_loader
-def load_user(id):
-    return USERS.get(int(id))
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
