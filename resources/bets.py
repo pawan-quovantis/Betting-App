c5e70cbaff9ed7c828427b0169bd35f7c25326e9
Vivek <vivek@quovantis.com>
2017-04-01 05:11:06 +0530

    missing check in Post call added.

--- resources/bets.py
+++ resources/bets.py
@@ -3,6 +3,7 @@ from models import *
 from flask import jsonify
 import models
 from flask import request
+from sqlalchemy import or_
 
 
 class Bets(Resource):
@@ -45,10 +46,16 @@ class Bets(Resource):
             bet = db.session.query(models.Bets).filter(models.Bets.user_id == user.id, models.Bets.fixture_id == fixture_id).first()
             if bet is None:
                 try:
-                    new_bet = Bets(user_id=user["id"], fixture_id=fixture_id, amount=amount, team_id=team_id)
-                    db.session.add(new_bet)
-                    db.session.commit()
-                    return jsonify({"Message": "Bet Placed Successfully", "Status-Code": 200})
+                    match_exists = db.session.query(Fixtures).filter(Fixtures.id == fixture_id,
+                                                                     or_(Fixtures.home_team_id == team_id,
+                                                                         Fixtures.away_team_id == team_id)).first()
+                    if match_exists is not None:
+                        new_bet = Bets(user_id=user["id"], fixture_id=fixture_id, amount=amount, team_id=team_id)
+                        db.session.add(new_bet)
+                        db.session.commit()
+                        return jsonify({"Message": "Bet Placed Successfully", "Status-Code": 200})
+                    else:
+                        return "Invalid Request"
                 except Exception as ex:
                     return ex.message, 500
             else:
